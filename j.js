c2be387a58050fee0d9bd41d3fdc77de8d89ce54
Bharath <mummadibharath9966@gmail.com>
2025-01-04 18:24:31 +0530

    Update j.js

--- j.js
+++ j.js
@@ -260,3 +260,10 @@ async function fetchSeriesByGenres(genreIds) {
         console.error('Error fetching series by genres:', error);
     }
 }
+
+document.getElementById('logoutButton').addEventListener('click', function () {
+    const confirmLogout = confirm('Are you sure you want to log out?');
+    if (confirmLogout) {
+        window.location.href = 'log.html'; // Redirect to the login page
+    }
+});
